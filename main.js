77d15bff262a4ec84d69e6ba33b5df5ba4818ec8
Matthew Kirby <matt.kirby@yahoo.com>
2020-10-29 19:10:47 -0400

    finished lab

--- main.js
+++ main.js
@@ -1,10 +1,45 @@
-// Defining text characters for the empty and full hearts for you to use later.
+// DOM ELEMENTS
+const articleContainer = document.querySelector("#articles-container")
+const likeHearts = document.querySelectorAll(".like-glyph")
+const modalBanner = document.querySelector("#modal")
+const modalMessageContainer = document.querySelector("p#modal-message")
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+// EVENT LISTENERS
+likeHearts.forEach(spanElement => {
+  spanElement.addEventListener("click", likeAPost)
+})
 
+// EVENT HANDLERS
 
+function likeAPost(event) {
+  const heartSpan = event.target
+  mimicServerCall("anyUrl-I-Think?.com")
+    .then(response => {
+      alternateHearts(heartSpan)
+      console.log(response)
+    })
+    .catch(error => {
+      displayError(error)
+    })
+}
+
+function displayError(error) {
+  modalMessageContainer.textContent = error
+  modalBanner.classList.remove("hidden")
+  setTimeout(() => modalBanner.classList.add("hidden"), 5000)
+}
+
+function alternateHearts(heartSpan) {
+  if (heartSpan.textContent === EMPTY_HEART) {
+    heartSpan.textContent = FULL_HEART
+    heartSpan.classList.add("activated-heart")
+  } else {
+    heartSpan.textContent = EMPTY_HEART
+    heartSpan.classList.remove("activated-heart")
+  }
+}
 
 
 //------------------------------------------------------------------------------
